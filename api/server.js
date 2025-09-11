6016296e1000a740bdc3fca439dfeaef36e93e91
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-09-11 19:03:54 -0300

    ajustado endereco de email e mensagens

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const { name, email, phone, message } = req.body;
 
     if (!name || !email || !phone || !message) {
-      return res.status(400).json({ error: "All fields are required!" });
+      return res.status(400).json({ error: "Preencha todos os campos!" });
     }
 
     try {
@@ -22,18 +22,18 @@ export default async function handler(req, res) {
 
       const mailOptions = {
         from: email,
-        to: "irfannayakvade2230@gmail.com",
-        subject: `Contact from ${name}`,
-        text: `You have a new message from ${name} (${email}, ${phone}):\n\n${message}`,
+        to: "gabrielabalbuquerque@outlook.com",
+        subject: `Entre em contato com ${name}`,
+        text: `Você recebeu uma mensagem de ${name} (${email}, ${phone}):\n\n${message}`,
       };
 
       await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Message sent successfully!" });
+      res.status(200).json({ message: "Mensagem enviada com sucesso!" });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Failed to send message" });
+      res.status(500).json({ error: "Falha ao enviar mensagem" });
     }
   } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+    res.status(405).json({ error: "Método não permitido" });
   }
 }
